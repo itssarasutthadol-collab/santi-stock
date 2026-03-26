082fc45aa87a5206a23fd379365f4340d5f18304
itssarasutthadol-collab <itssara.sutthadol@gmail.com>
2026-03-26 13:25:03 +0700

    Update supabase.js

--- santi-stock/src/supabase.js
+++ santi-stock/src/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // ⚠️ ใส่ค่าจาก Supabase Dashboard → Settings → API
 const SUPABASE_URL = 'https://rrhsuuwcuvuvobeqzxal.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_xWIHC25j2F0BmJ491uotNw_Kw_cMYMdl';
+const SUPABASE_KEY = 'sb_publishable_xWIHC25j2F0BmJ491uotNw_Kw_cMYMd';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
